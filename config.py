fcdae3e9853b67616bb293e94d70348fbd12c7a2
radhamusicbot <radhamusicbot@gmail.com>
2024-12-16 20:57:10 +0530

    Update config.py

--- config.py
+++ config.py
@@ -6,12 +6,12 @@ load_dotenv()
 
 API_ID = int(getenv("API_ID", "21803165"))
 API_HASH = getenv("API_HASH", "05e5e695feb30e25bef47484cc006da7")
-BOT_TOKEN = getenv("BOT_TOKEN", None)
-OWNER_ID = int(getenv("OWNER_ID", "7403621976"))
-MONGO_URL = getenv("MONGO_URL", None)
-SUPPORT_GRP = getenv("SUPPORT_GRP", "Purvi_UPdates")
-UPDATE_CHNL = getenv("UPDATE_CHNL", "WORLD_ALPHA")
-OWNER_USERNAME = getenv("OWNER_USERNAME", "ll_ALPHA_BABY_lll")
+BOT_TOKEN = getenv("BOT_TOKEN", "8171600424:")
+OWNER_ID = int(getenv("OWNER_ID", "5909658683"))
+MONGO_URL = getenv("MONGO_URL", "mongodb+srv://Villainmusic01:deathnote0p@cluster0.nah8e.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+SUPPORT_GRP = getenv("SUPPORT_GRP", "oldskoolgc")
+UPDATE_CHNL = getenv("UPDATE_CHNL", "iamvillain77")
+OWNER_USERNAME = getenv("OWNER_USERNAME", "iamakki001")
 
 # Random Start Images
 IMG = [
